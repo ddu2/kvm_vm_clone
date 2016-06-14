ba17fe135118091ff512acb9e66fbb55dddd9f96
ddu2 <dudp@foxmail.com>
2016-06-14 12:20:36 +0800

    Add files via upload

--- vm-clone.sh
+++ vm-clone.sh
@@ -3,19 +3,14 @@
 echo "======"
 virsh list --all | grep tpl | awk '{print $2}' | nl | sed 's/^   //g'
 echo "======"
+
 echo -e "Choose the template: \c"
-#read template
 read tpl_num
 
-case $tpl_num in
-    "1") template="a00-rhel65-tpl" ;;
-    "2") template="a00-rhel67-tpl" ;;
-    "3") template="a00-rhel72-atomic-tpl" ;;
-    "4") template="a00-rhel72-tpl" ;;
-    * )  echo "Please enter [1..4]."; exit;;
-esac
+template=`virsh list --all | grep tpl | nl | sed -n ${tpl_num}p | awk '{print $3}'`
 
 echo -e "Enter the new vm name: \c"
 read vm_name
 
+#echo $template, $vm_name
 /usr/bin/virt-clone -o $template -n $vm_name --auto
